36594fd4c78a63e0ef5fe71ea21e84e8cb202a19
Roy Rim <rrim@silversky.com>
2014-03-08 18:10:48 -0500

    switched to use jersey-spring, include sample autowired

---
+++ src/main/java/com/example/transaction/TransactionBo.java
@@ -0,0 +1,7 @@
+package com.example.transaction;
+
+public interface TransactionBo {
+
+	String save();
+	
+}
---
+++ src/main/java/com/example/transaction/impl/TransactionBoImpl.java
@@ -0,0 +1,11 @@
+package com.example.transaction.impl;
+
+import com.example.transaction.TransactionBo;
+
+public class TransactionBoImpl implements TransactionBo {
+
+	public String save() {
+		return "whatever";
+	}
+
+}
